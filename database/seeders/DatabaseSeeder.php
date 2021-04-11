ec8c99712fc1ebd4d3530c29fe80aa2ee849b93a
mifaou <oumaima.ouhaik@hotmail.com>
2021-04-11 13:13:12 +0000

    Add Interface + Structure

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        // $this->call(RoleSeeder::class);
+        // $this->call(UserSeeder::class);
+         $this->call(AdminSeeder::class);
+        //  Company::factory(20)->create();
+        //  User::factory(1)->create();
     }
 }
