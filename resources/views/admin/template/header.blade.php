c5ba5f74d5fd74570c4d8f80a194bc9629e02239
mifaou <oumaima.ouhaik@hotmail.com>
2021-04-11 14:29:42 +0000

    Update Admin

--- resources/views/admin/template/header.blade.php
+++ resources/views/admin/template/header.blade.php
@@ -1,7 +1,7 @@
 <div class="slim-header with-sidebar">
     <div class="container-fluid">
       <div class="slim-header-left">
-      <h2 class="slim-logo"><a href="{{route('dashboard')}}">CRM</a><span>.</span></a></h2>
+      <h2 class="slim-logo"><a href="{{route('dashboard')}}">CRM Project</a><span></span></a></h2>
         <a href="" id="slimSidebarMenu" class="slim-sidebar-menu"><span></span></a>
         {{-- <div class="search-box">
           <input type="text" class="form-control" placeholder="Search">
@@ -12,16 +12,13 @@
 
         <div class="dropdown dropdown-c">
           <a href="#" class="logged-user" data-toggle="dropdown">
-            <img src="http://via.placeholder.com/500x500" alt="">
+            <img src="https://img.icons8.com/bubbles/2x/admin-settings-male.png" alt="">
           <span>{{Auth::user()->name}}</span>
             <i class="fa fa-angle-down"></i>
           </a>
           <div class="dropdown-menu dropdown-menu-right">
             <nav class="nav">
-              {{-- <a href="#" class="nav-link"><i class="icon ion-person"></i> View Profile</a>
-              <a href="#" class="nav-link"><i class="icon ion-compose"></i> Edit Profile</a>
-              <a href="#" class="nav-link"><i class="icon ion-ios-bolt"></i> Activity Log</a>
-              <a href="#" class="nav-link"><i class="icon ion-ios-gear"></i> Account Settings</a> --}}
+
               <a href="{{ route('admin.logout') }}" class="nav-link"><i class="icon ion-forward"></i> Sign Out</a>
             </nav>
           </div><!-- dropdown-menu -->
